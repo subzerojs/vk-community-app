6219b909afdfa11e66e8ff401d087cb8573211f4
bakakasin <bakakasin@gmail.com>
2017-11-20 04:37:58 +0700

    update

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ var app = {
       }
 }   
   VK.init(function() {
-    VK.callMethod("showAppWidgetPreviewBox", 'table', 'return ' + JSON.stringify(data) + ';') 
+    
 document
   .getElementById('update-w')
   .addEventListener('click', function(){
@@ -74,7 +74,7 @@ document
         .getElementById('add-w')
         .addEventListener('click', function(e){
           //e.preventDefault();
-         // VK.callMethod("showGroupSettingsBox", +65);
+          VK.callMethod("showGroupSettingsBox", +65);
          
         VK.callMethod("showAppWidgetPreviewBox", 'table', 'return ' + JSON.stringify(data) + ';') 
           VK.addCallback('onSettingsChanged', function(){
